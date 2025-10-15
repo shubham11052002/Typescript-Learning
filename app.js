3319d34c9e1ca45fe158fba25653eb014402e761
shubhamsharma21505 <shubham.s@tekanthem.com>
2025-10-15 14:29:28 +0530

    learning custom type & create

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-function getTotal(numbers) {
-    return numbers.reduce(function (acc, item) {
-        return acc + item;
-    }, 0);
-}
-console.log(getTotal([2, 3, 4,]));
-var name1 = 'Shubham Sharma';
-console.log(name1);
-var arr = [4, 5, 6];
-arr.map(function (items) {
-    console.log(items);
-});
+/*
+  function getTotal(numbers:number[]){
+  return numbers.reduce((acc,item)=>{
+      return acc + item;
+  },0);
+  }
+
+  console.log(getTotal([2,3,4,]));
+
+  let name1:string = 'Shubham Sharma';
+  console.log(name1);
+  const arr:Array<number>=[4,5,6];
+  arr.map((items)=>{
+      console.log(items);
+  });
+  
+
+  // Printing table by function
+  let val1:number = 10;
+  function printTable(val1:number){
+    for(let i:number=1;i<=10;i++){
+      console.log(val1 + " * " + i + " = " + val1*i);
+    }
+  }
+  printTable(val1);  */
+var userData = {
+    name: '12',
+    age: 12,
+    mob: 2987482,
+};
+console.log(userData);
--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-
+  /*
     function getTotal(numbers:number[]){
     return numbers.reduce((acc,item)=>{
         return acc + item;
@@ -14,3 +14,27 @@
         console.log(items);
     });
     
+
+    // Printing table by function
+    let val1:number = 10;
+    function printTable(val1:number){
+      for(let i:number=1;i<=10;i++){
+        console.log(val1 + " * " + i + " = " + val1*i);
+      }
+    }
+    printTable(val1);  */
+
+
+    // type alias
+    type User = {
+      name:string;
+      age:number;
+      mob?:number,
+    }
+
+    const userData: User = {
+      name:'12',
+      age:12,
+      mob:2987482,
+    }
+    console.log(userData);
