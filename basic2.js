000f1dba6919ff8a2d812b1394b888bedcd2b670
shubhamsharma21505 <shubham.s@tekanthem.com>
2025-10-21 22:14:28 +0530

    for loop in TS

--- basic2.js
+++ basic2.js
@@ -7,3 +7,21 @@ var emp2 = {
 console.log(emp2);
 var objResult = { location: "ghaziabad", email: "shubham@gmail.com", objId: 28734 };
 console.log(objResult);
+function abc(a, b) {
+    if (typeof a === "number" && typeof b === "undefined") {
+        console.log("hello this is running abc 1st function");
+    }
+    else if (typeof a === "number" && typeof b === "string") {
+        console.log("hello this is running abc 2nd function");
+    }
+    else {
+        console.log("Something went wrong");
+    }
+}
+abc(12);
+abc(12, "Shubham Sharma");
+// FOR LOOP
+var i2 = 10;
+for (var i = 1; i <= i2; i++) {
+    console.log(i2 + " * " + i + " = " + i2 * i);
+}
--- basic2.ts
+++ basic2.ts
@@ -30,3 +30,25 @@ interface obj2 extends obj1 {
 let objResult: obj2 = { location: "ghaziabad", email: "shubham@gmail.com", objId: 28734 };
 console.log(objResult);
 
+// function overloading
+function abc(a: number): void;
+function abc(a: number, b: string): number | string;
+
+function abc(a: any, b?: any): void | number | string {
+    if (typeof a === "number" && typeof b === "undefined") {
+        console.log("hello this is running abc 1st function");
+    } else if (typeof a === "number" && typeof b === "string") {
+        console.log("hello this is running abc 2nd function");
+    } else {
+        console.log("Something went wrong");
+    }
+}
+abc(12);
+abc(12, "Shubham Sharma")
+
+
+// FOR LOOP
+let i2: number = 10;
+for (let i: number = 1; i <= i2; i++) {
+    console.log(i2 + " * " + i + " = " + i2 * i);
+}
