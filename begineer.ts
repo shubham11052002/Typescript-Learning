9609aa66099bb23e364cf16f7ff38d2bd5e032dc
shubhamsharma21505 <shubham.s@tekanthem.com>
2025-10-22 20:44:34 +0530

    conditonal operator in Typescript

---
+++ begineer.ts
@@ -0,0 +1,10 @@
+let age = 10.10;
+if (age < 18) {
+    console.log("you cannot you are under 18`s");
+} else if (age >= 18 && age > 91) {
+    console.log("you can vote. Go and vote First");
+} else if (age > 90) {
+    console.log("you cannot vote you are beyond 90`s");
+} else {
+    console.log("Invalid age");
+}
